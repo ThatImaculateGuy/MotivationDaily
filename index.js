bf5a9c5b635db7d3331cbfa2328efec621738430
ThatImaculateGuy <106443948+ThatImaculateGuy@users.noreply.github.com>
2022-05-30 12:10:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ console.log(";P")
 var device;
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-        var device = "mobile";
+        device = "mobile";
         console.log("mobile");
+        alert("MOBILE")
   }else{
     // false for not mobile device
-        var device = "computer";
+       device = "computer";
         console.log("computer");
+        alert("COMPUTER")
   }
   if (device == "mobile") {
     device.setAttribute('href', 'mobile.css');
