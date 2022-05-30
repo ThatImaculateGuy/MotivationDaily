2d98419fe6e53df71d584434da6ec676eb458030
ThatImaculateGuy <106443948+ThatImaculateGuy@users.noreply.github.com>
2022-05-29 17:18:23 -0700

    Better Compatibility with Mobile Devices
    -Bug Fixes:
    Fixed Background glitching
    Low Res Background
    
    New things:
    Separate CSS files for mobile devices
    JS file

---
+++ index.js
@@ -0,0 +1,15 @@
+var device;
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+        var device = "mobile";
+        console.log("mobile");
+  }else{
+    // false for not mobile device
+        var device = "computer";
+        console.log("computer");
+  }
+  if (device == "mobile") {
+    device.setAttribute('href', 'mobile.css');
+  }else {
+    
+  }
